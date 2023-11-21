771cae6004e80eedbdd33dc23c3f77a2da86ebab
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-11-20 22:41:38 -0300

    update

--- hooks-tsx/custom-hook-tsx/src/App.tsx
+++ hooks-tsx/custom-hook-tsx/src/App.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import videoSrc from "./assets/video.mp4";
+import useLocalStorage from "./useLocalStorage";
 
 function App() {
   const video = React.useRef<HTMLVideoElement>(null); // Está referenciando o elemento video, dizendo que ele é do tipo HTMLVideoElement e que ele começa com o valor null.
+  const [volume, setVolume] = useLocalStorage("volume", "0"); // Está chamando o hook personalizado useLocalStorage e passando como parâmetro uma chave e um valor inicial, sendo ambos do tipo string e retornando um array sendo o primeiro elemento uma string e o segundo elemento uma função atualizadora.
+
+  // Criado um efeito colateral que é executado toda vez que o volume mudar.
+  React.useEffect(() => {
+    if (!video.current) return; // Se o vídeo não existir, não retorna nada, fazendo com que o código abaixo não seja executado.
+    
+    const volumeNumber = Number(volume); // Está convertendo o volume para number e atribuindo para a variável volumeNumber.
+
+    // Se o volume for maior ou igual a 0 e menor ou igual a 100, executa o if.
+    if (volumeNumber >= 0 && volumeNumber <= 100) {
+      video.current.volume = volumeNumber / 100; // Está setando o volume do vídeo com o valor do estado dividido por 100, pelo fato do volume do vídeo ser de 0 a 1.
+    }
+  }, [volume]);
 
   return (
     <div>
       <div className="flex">
-        <button>0</button>
-        <button>50</button>
-        <button>100</button>
+        <button onClick={() => setVolume("0")}>0</button>
+        <button onClick={() => setVolume("50")}>50</button>
+        <button onClick={() => setVolume("100")}>100</button>
+        <button onClick={() => setVolume("200")}>200</button>
       </div>
 
       {/* O ref está referênciado e passando o elemento para a variável video, para que possamos manipular esse elemento. */}
--- hooks-tsx/custom-hook-tsx/src/useLocalStorage.tsx
+++ hooks-tsx/custom-hook-tsx/src/useLocalStorage.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const useLocalStorage = (key: string, inicial: string) => {
-  return <div>useLocalStorage</div>;
+// Criado um hook personalizado chamado useLocalStorage que recebe como parâmetro uma chave e um valor inicial, sendo ambos do tipo string(pois o localStorage só aceita string) e retornando um array sendo o primeiro elemento uma string e o segundo elemento uma função atualizadora.
+const useLocalStorage = (
+  key: string,
+  inicial: string,
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  // Criado um estado chamado state e uma função atualizadora chamada setState. O estado inicial é uma função que retorna a chave passada como parâmetro ou o valor inicial caso a chave não exista.
+  const [state, setState] = React.useState(() => {
+    const local = window.localStorage.getItem(key); // Está pegando o item do localStorage com a chave passada como parâmetro.
+    return local ? local : inicial; // Se o local existir, retorna o local, se não retorna o valor inicial.
+  });
+
+  // Criado um efeito colateral que é executado toda vez que o estado ou a chave mudar.
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state); // Está setando um item no localStorage com a chave passada como parâmetro e o valor do estado.
+  }, [state, key]);
+
+  return [state, setState]; // Retorna uma array com o estado e a função atualizadora.
 };
 
 export default useLocalStorage;
